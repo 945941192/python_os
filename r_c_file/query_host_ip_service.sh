0683027f7eee77228ea565c83fc6fb1dc9517e7d
weizhanbiao <wb-wzb308601@alibaba-inc.com>
2017-08-28 14:20:04 +0800

    add shell and python c_r file query houstname or ip docker services

--- r_c_file/query_host_ip_service.sh
+++ r_c_file/query_host_ip_service.sh
@@ -1,18 +1,33 @@
 #! /bin/bash
 
 in_info=''
-re_ip_name="^[0-9]+.[0-9]+.[0-9]+.[0-9]*$"
 
 ##############  Function ########################
 
+error()
+{
+    local err=$1
+    echo ""
+    echo "$err"
+    exit -1
+}
+
 query_services()
 { 
-    if [ $1 =~ re_ip_name ];then
-        local ip_info=$1 
-        ip_info=$(cat rtable.csv | grep $1 | awk -F ',' '{print $7}')
-    else
-        local host_name_info=$1
-        host_name=$
+        host_or_ip_info=$1 
+        status1=$(grep $1 rtable.csv | wc -l) 
+        [ ${status1} -eq 0 ] && error "No query to the host"
+        data1=$(grep $1 rtable.csv | awk -F ',' '{print $7}')
+        new_appgroup=$(grep $1 rtable.csv | awk -F ',' '{print $3}')
+        printf  "host ${data1} new_appgroup ${new_appgroup}\n"
+        status2=$(grep $1 container_arrangement.csv | wc -l)
+        [ ${status2} -ne 0 ] && for info in $(grep $1 container_arrangement.csv)
+        do
+            service_instance=$(echo ${info} | awk -F ',' '{print $1}')
+            printf  "        |\n"
+            printf  "        |\n"
+            printf  "        ----dockerservice:${service_instance}\n"
+        done
 }
 
 
@@ -27,9 +42,11 @@ usage()
 {
     cat << EOF
     Usage:
-        $(basename $0) -t <test Domain name>        Check DNS server by Domain name
+        $(basename $0) -q <query hostname or ip>        Query hostname or ip 
     Example:
-        $(basename $0) -t ntp.aliyun.com
+        $(basename $0) -q 10.1.7.32
+        or
+        $(basename $0) -q rc85c6128.cloud.nu17
 EOF
     exit 1
 }
@@ -37,10 +54,10 @@ EOF
 
 [ $# -eq 0 ] && usage
 
-while getopts t: OPTION
+while getopts q: OPTION
 do
     case $OPTION in
-        t) in_info="$OPTARG";;
+        q) in_info="$OPTARG";;
         *) usage;;
     esac
 done
