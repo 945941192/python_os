8dba4ea95aad47aa1f3192e44dc035c61684a373
weizhanbiao <wb-wzb308601@alibaba-inc.com>
2017-08-30 11:14:53 +0800

    complete query docker 2

--- r_c_file/query_host_ip_service_2.sh
+++ r_c_file/query_host_ip_service_2.sh
@@ -22,37 +22,34 @@ query_services()
         local data_c=$(grep -w $1 ${c_file_path})
         local status_r=$(echo ${data_r} | wc -w) 
         local status_c=$(echo ${data_c} | wc -w)
-        #[ ${status1} -eq 0 ] && error "No query to the host"
         [ ${status_r} -ne 0 ] && r_success=1
         [ ${status_c} -ne 0 ] && c_success=1
-        
-######## data true or fals ########
         [ ${r_success} -ne 1 ] && [ ${c_success} -ne 1 ] && error "No query to the host"
         [ ${r_success} -ne 1 ] && [ ${c_success} -eq 1 ] && data_r=$(grep -w $(grep -w $1 ${c_file_path} | head -1 | awk -F ',' '{print $9}') ${r_file_path})
-        
-######print r
-        local ip=$(echo ${data_r} | awk -F ',' '{print $7}')
-        local hostname=$(echo ${data_r} | awk -F ',' '{print $6}')
-        local product=$(echo ${data_r} | awk -F ',' '{print $2}')
-        local new_appgroup=$(echo ${data_r} | awk -F ',' '{print $3}')
-        local node=$(echo ${data_r} | awk -F ',' '{print $4}')
-        local OS=$(echo ${data_r} | awk -F ',' '{print $8}')
-        local new_appgroup=$(echo ${data_r} | awk -F ',' '{print $3}')
-        printf  "hostnam(\t\E[1;31;33m${hostname}\E[0m) IP(\t\E[1;31;33m${ip}\E[0m) OS(${OS}) product(${product}) new_appgroup(${new_appgroup}) node(${node})     \n"
-   
-######print c
-        for info in ${data_c}
+        output_host_info "${data_r}" "${data_c}"
+}
+
+
+output_host_info()
+{
+        local ip=$(echo $1 | awk -F ',' '{print $7}')
+        local hostname=$(echo $1 | awk -F ',' '{print $6}')
+        local product=$(echo $1 | awk -F ',' '{print $2}')
+        local new_appgroup=$(echo $1 | awk -F ',' '{print $3}')
+        local node=$(echo $1 | awk -F ',' '{print $4}')
+        local OS=$(echo $1 | awk -F ',' '{print $8}')
+        local new_appgroup=$(echo $1 | awk -F ',' '{print $3}')
+        printf  "Host computer: hostnam(\E[1;31;33m${hostname}\E[0m) IP(\E[1;31;33m${ip}\E[0m) OS(${OS}) \n"
+        printf  "               product(\E[1;31;33m${product}\E[0m) new_appgroup(\E[1;31;33m${new_appgrop}\E[0m) node(\E[1;31;33m${node}\E[0m) \n"
+        for info in $2
         do
             local service_instance=$(echo ${info} | awk -F ',' '{print $1}')
             local docker_hostname=$(echo ${info} | awk -F ',' '{print $5}')
             local docker_ip=$(echo ${info} | awk -F ',' '{print $4}')
-            
-            printf  "        |\n"
-            printf  "        ----docker_hostname(\t\E[1;31;32m${docker_hostname}\E[0m) docker_ip(${docker_ip})  dockerservice(${service_instance})\n"
+            printf  "    ----Docker: hostname(\E[1;31;32m${docker_hostname}\E[0m) IP(${docker_ip})  service(${service_instance})\n"
         done
+
 }
-
-
 
 ################End Function#####################
 
--- r_c_file/query_host_ip_service_3.sh
+++ r_c_file/query_host_ip_service_3.sh
@@ -39,13 +39,14 @@ output_host_info()
         local node=$(echo $1 | awk -F ',' '{print $4}')
         local OS=$(echo $1 | awk -F ',' '{print $8}')
         local new_appgroup=$(echo $1 | awk -F ',' '{print $3}')
-        printf  "hostnam(\E[1;31;33m${hostname}\E[0m) IP(\E[1;31;33m${ip}\E[0m) OS(${OS}) product(${product}) new_appgroup(${new_appgroup}) node(${node})     \n"
+        printf  "hostnam(\E[1;31;33m${hostname}\E[0m) IP(\E[1;31;33m${ip}\E[0m) OS(${OS}) \n"
+        printf  "product(\E[1;31;33m${product}\E[0m) new_appgroup(\E[1;31;33m${new_appgrop}\E[0m) node(\E[1;31;33m${node}\E[0m) \n"
         for info in $2
         do
             local service_instance=$(echo ${info} | awk -F ',' '{print $1}')
             local docker_hostname=$(echo ${info} | awk -F ',' '{print $5}')
             local docker_ip=$(echo ${info} | awk -F ',' '{print $4}')
-            printf  "        ----docker_hostname(\E[1;31;32m${docker_hostname}\E[0m) docker_ip(${docker_ip})  dockerservice(${service_instance})\n"
+            printf  "        ---Docker hostname(\E[1;31;32m${docker_hostname}\E[0m) IP(${docker_ip})  service(${service_instance})\n"
         done
 
 }
