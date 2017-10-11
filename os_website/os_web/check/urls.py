abf146ff7d795d94caf29e1f314d18f224730568
weizhanbiao <wb-wzb308601@alibaba-inc.com>
2017-10-11 13:40:00 +0800

    a bug wait me

--- os_website/os_web/check/urls.py
+++ os_website/os_web/check/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.upload_check_tarball, name='upload_check_tarball'),
+    url(r'^$', views.handle_upload_checktarball, name='upload_check_tarball'),
+    url(r'^test$', views.test, name='test'),
 ]
--- os_website/os_web/check/views.py
+++ os_website/os_web/check/views.py
@@ -1,12 +1,23 @@
 #coding=utf-8
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
+from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import csrf_exempt
 
-
-def upload_check_tarball(request):
+@csrf_exempt
+def test(request):
+#    return render(request,"check/upload_check_tarball.html")
+    return HttpResponse("nicai")
+    
+@csrf_exempt
+def handle_upload_checktarball(request):
 #    return HttpResponse("hello,world,you are very niubi!!!")
-    return render(request,"check/upload_check_tarball.html")
+    if request.method == "GET":
+        return render(request,"check/upload_check_tarball.html")
+    elif request.method == "POST":
+        print "*"*9
+        return HttpResponse("nicai")
 
 
 
