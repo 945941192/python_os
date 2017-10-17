0bb3a4483653614c9493377f31d2f24761c36d16
weizhanbiao <wb-wzb308601@alibaba-inc.com>
2017-10-17 18:23:58 +0800

    add script tool modify html

--- os_website/os_web/check/views.py
+++ os_website/os_web/check/views.py
@@ -19,7 +19,6 @@ def handle_upload_checktarball(request):
         return render(request,"check/upload_check_tarball.html",{"tarball_set":tarball_set})
     if request.method == "POST":
         tarball = request.FILES.get("tarball",None)
- #保存tarball到/var/log       
         path_status = os.path.exists(FilePath)
         if not path_status:
             os.makedirs(FilePath)
@@ -27,14 +26,12 @@ def handle_upload_checktarball(request):
         with open(full_path_tarball,"w") as pic:
             for c in tarball.chunks():
                 pic.write(c)
-######check tar_ball
+#check tar_ball
         cld_check_script_path = subprocess.check_output("find %s -name cld_check.sh"%PROJEKT_DIR,shell=True).strip()
         check_status = subprocess.call("sh %s -c -f  %s"%(cld_check_script_path,full_path_tarball),shell=True) 
-
-######updown check report 
+#updown check report 
         tar_x_tarball = subprocess.call("cd %s && tar -zxf %s"%(FilePath,full_path_tarball),shell=True)
-
-######将上传的ball信息保存
+#save
         tarball_obj = UploadTarBallInfo()
         tarball_obj.file_name = tarball.name
         tarball_obj.up_status = 1
@@ -77,13 +74,13 @@ def handle_down_check_report(request):
 @csrf_exempt
 def handle_script_tools(request):
     if request.method == "GET":
-        tool_name = request.GET.get("tool_name","")
+        tool_id = request.GET.get("tool_id")
+        tool_name = request.GET.get("tool_name","").strip()
         tool_instructions = request.GET.get("tool_instructions","")
         down = int(request.GET.get("down",0))
         save = int(request.GET.get("save",0))
+        mod = int(request.GET.get("mod",0))
         dele = int(request.GET.get("del",0))
-        print "tool_name======",tool_name,save
-        print "down========",down
         if down == 1:
             file_name = subprocess.check_output("find %s -name %s"%(PROJEKT_DIR,tool_name),shell=True).strip()
             def readFile(fn,buf_size=262144):
@@ -99,7 +96,6 @@ def handle_script_tools(request):
             response['Content-Disposition'] = 'attachment;filename=%s'%file_name
             response['Content-Length'] = os.path.getsize(file_name)
             return response
-
         elif save == 1:
             tool_obj = ScriptToolsInfo()
             tool_obj.tool_name = tool_name
@@ -108,6 +104,12 @@ def handle_script_tools(request):
             return HttpResponseRedirect(reverse("check:script_tools"))
         elif dele == 1:
             ScriptToolsInfo.objects.filter(tool_name=tool_name).delete()
+            return HttpResponseRedirect(reverse("check:script_tools"))
+        elif mod == 1:
+            tool_obj = ScriptToolsInfo.objects.get(id=int(tool_id))
+            tool_obj.tool_name = tool_name
+            tool_obj.tool_instructions = tool_instructions
+            tool_obj.save()
             return HttpResponseRedirect(reverse("check:script_tools"))
         else:
             script_tools_set = ScriptToolsInfo.objects.all()
